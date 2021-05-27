9a163a925d55cb5080303cd96e2b5e231519ed34
HY <hymin1224@naver.com>
2021-05-27 14:58:31 +0900

    ♻️  position: sticky; & position: fixed; + z-index setting

--- js/sideGnb.js
+++ js/sideGnb.js
@@ -4,9 +4,11 @@ const open = document.querySelector('.open')
 const close = document.querySelector('.close')
 const sideGnb = document.querySelector('.side-gnb')
 const globalHeader = document.querySelector('.global-header')
+const commonBackground = document.querySelector('.common-background')
 
 if (mediaDesktop.matches) {
   open.classList.add('visually-hidden')
+  commonBackground.classList.add('is-active')
   sideGnb.classList.add('is-active')
   globalHeader.classList.add('is-active')
 } else {
@@ -14,6 +16,7 @@ if (mediaDesktop.matches) {
 }
 
 toggleMenuBtn.addEventListener('click', () => {
+  commonBackground.classList.toggle('is-active')
   sideGnb.classList.toggle('is-active')
   globalHeader.classList.toggle('is-active')
   open.classList.toggle('visually-hidden')
@@ -23,11 +26,13 @@ toggleMenuBtn.addEventListener('click', () => {
 /* Desktop Media Query Change Event */
 window.addEventListener('resize', () => {
   if (mediaDesktop.matches) {
+    commonBackground.classList.add('is-active')
     sideGnb.classList.add('is-active')
     globalHeader.classList.add('is-active')
     open.classList.add('visually-hidden')
     close.classList.remove('visually-hidden')
   } else {
+    commonBackground.classList.remove('is-active')
     sideGnb.classList.remove('is-active')
     globalHeader.classList.remove('is-active')
     open.classList.remove('visually-hidden')
