d89f7a542e102196eb0f5da3824958613e0df821
HY <hymin1224@naver.com>
2021-06-12 15:50:52 +0900

    ♻️  Update modal & forms

--- js/modal.js
+++ js/modal.js
@@ -1,5 +1,6 @@
 const modals = document.querySelectorAll('.modal')
 const deleteBtns = document.querySelectorAll('.delete-btn')
+const deleteBasicBtn = document.querySelector('.delete-b-btn')
 const deleteBoardModal = document.querySelector('.delete-board-modal')
 const completeModal = document.querySelector('.complete-modal')
 const completeDeleteBoardModal = document.querySelector(
@@ -15,13 +16,15 @@ const againEditBoardModal = document.querySelector('.again-edit-board-modal')
 const detailBoardModal = document.querySelector('.detail-board-modal')
 const cancelBoardModal = document.querySelector('.cancel-board-modal')
 const editBoardModal = document.querySelector('.edit-board-modal')
-const cancelBoardBtn = document.querySelector('.cancle-board-btn')
+const secondModal = document.querySelector('.second-modal')
+const cancelBoardBtn = document.querySelector('.cancel-board-btn')
 const deleteBoardBtn = document.querySelector('.delete-board-btn')
 const editBoardBtn = document.querySelector('.edit-board-btn')
 const backBtns = document.querySelectorAll('.back-btn')
 const editBtns = document.querySelectorAll('.edit-btn')
 const confirmBtn = document.querySelector('.confirm-btn')
-const completeBtn = document.querySelector('.complete-btn')
+const completeBtns = document.querySelectorAll('.complete-btn')
+const basicBtns = document.querySelectorAll('.btn-basic')
 const modalTr = document.querySelectorAll('.click-detail-modal tr')
 const overlay = document.querySelector('.overlay')
 const body = document.querySelector('body')
@@ -34,6 +37,18 @@ if (deleteBtns) {
       deleteBoardModal.classList.add('is-active')
     })
   }
+}
+
+if (deleteBasicBtn) {
+  deleteBasicBtn.addEventListener('click', () => {
+    overlay.classList.add('is-active')
+    body.classList.add('modal-open')
+    deleteBoardModal.classList.add('is-active')
+
+    for (const basicBtn of basicBtns) {
+      basicBtn.disabled = 'disabled'
+    }
+  })
 }
 
 if (editBtns) {
@@ -59,6 +74,10 @@ if (editBoardBtn) {
     overlay.classList.add('is-active')
     body.classList.add('modal-open')
     againEditBoardModal.classList.add('is-active')
+
+    for (const basicBtn of basicBtns) {
+      basicBtn.disabled = 'disabled'
+    }
   })
 }
 
@@ -81,15 +100,19 @@ for (const tr of modalTr) {
 for (const backBtn of backBtns) {
   backBtn.addEventListener('click', () => {
     let isActive = false
-    if (againEditBoardModal) {
-      for (const className of againEditBoardModal.classList) {
+    if (secondModal) {
+      for (const className of secondModal.classList) {
         if (className == 'is-active') {
           isActive = true
         }
       }
     }
     if (isActive) {
-      againEditBoardModal.classList.remove('is-active')
+      secondModal.classList.remove('is-active')
+
+      for (const basicBtn of basicBtns) {
+        basicBtn.disabled = false
+      }
     } else {
       overlay.classList.remove('is-active')
       body.classList.remove('modal-open')
@@ -104,27 +127,36 @@ if (deleteBoardBtn) {
   deleteBoardBtn.addEventListener('click', () => {
     body.classList.add('modal-open')
     deleteBoardModal.classList.remove('is-active')
-    completeModal.classList.add('is-active')
+    completeDeleteBoardModal.classList.add('is-active')
+    completeInputBoardModal.classList.remove('is-active')
+
+    // completeModal.classList.add('is-active')
   })
 }
 
-if (completeBtn) {
-  completeBtn.addEventListener('click', () => {
-    body.classList.remove('modal-open')
-    overlay.classList.remove('is-active')
-    for (const modal of modals) {
-      modal.classList.remove('is-active')
-    }
-    completeInputBoardModal.classList.remove('is-active')
-  })
+if (completeBtns) {
+  for (const completeBtn of completeBtns)
+    completeBtn.addEventListener('click', () => {
+      body.classList.remove('modal-open')
+      overlay.classList.remove('is-active')
+      for (const modal of modals) {
+        modal.classList.remove('is-active')
+      }
+      // completeInputBoardModal.classList.remove('is-active')
+      // completeDeleteBoardModal.classList.remove('is-active')
+    })
 }
 
 overlay.addEventListener('click', () => {
   overlay.classList.remove('is-active')
   body.classList.remove('modal-open')
-  // completeModal.classList.remove('is-active')
+  completeModal.classList.remove('is-active')
   for (const modal of modals) {
     modal.classList.remove('is-active')
+  }
+
+  for (const basicBtn of basicBtns) {
+    basicBtn.disabled = false
   }
 })
 
