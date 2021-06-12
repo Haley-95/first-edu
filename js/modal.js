4fd90a0f3bbab423b8024f3235b147019afd0a38
HY <hymin1224@naver.com>
2021-06-12 10:33:09 +0900

    ♻️ Update storage-input / list modal & detail page

--- js/modal.js
+++ js/modal.js
@@ -8,11 +8,18 @@ const completeDeleteBoardModal = document.querySelector(
 const completeInputBoardModal = document.querySelector(
   '.complete-input-board-modal'
 )
+const completeEditBoardModal = document.querySelector(
+  '.complete-edit-board-modal'
+)
+const againEditBoardModal = document.querySelector('.again-edit-board-modal')
 const detailBoardModal = document.querySelector('.detail-board-modal')
-const cancelBoardModal = document.querySelector('.cancel-board-mordal')
-const cancelBtn = document.querySelector('.cancle-btn')
+const cancelBoardModal = document.querySelector('.cancel-board-modal')
+const editBoardModal = document.querySelector('.edit-board-modal')
+const cancelBoardBtn = document.querySelector('.cancle-board-btn')
 const deleteBoardBtn = document.querySelector('.delete-board-btn')
+const editBoardBtn = document.querySelector('.edit-board-btn')
 const backBtns = document.querySelectorAll('.back-btn')
+const editBtns = document.querySelectorAll('.edit-btn')
 const confirmBtn = document.querySelector('.confirm-btn')
 const completeBtn = document.querySelector('.complete-btn')
 const modalTr = document.querySelectorAll('.click-detail-modal tr')
@@ -20,8 +27,8 @@ const overlay = document.querySelector('.overlay')
 const body = document.querySelector('body')
 
 if (deleteBtns) {
-  for (const btn of deleteBtns) {
-    btn.addEventListener('click', () => {
+  for (const deleteBtn of deleteBtns) {
+    deleteBtn.addEventListener('click', () => {
       overlay.classList.add('is-active')
       body.classList.add('modal-open')
       deleteBoardModal.classList.add('is-active')
@@ -29,11 +36,29 @@ if (deleteBtns) {
   }
 }
 
-if (cancelBtn) {
-  cancelBtn.addEventListener('click', () => {
+if (editBtns) {
+  for (const editBtn of editBtns) {
+    editBtn.addEventListener('click', () => {
+      overlay.classList.add('is-active')
+      body.classList.add('modal-open')
+      editBoardModal.classList.add('is-active')
+    })
+  }
+}
+
+if (cancelBoardBtn) {
+  cancelBoardBtn.addEventListener('click', () => {
     overlay.classList.add('is-active')
     body.classList.add('modal-open')
     cancelBoardModal.classList.add('is-active')
+  })
+}
+
+if (editBoardBtn) {
+  editBoardBtn.addEventListener('click', () => {
+    overlay.classList.add('is-active')
+    body.classList.add('modal-open')
+    againEditBoardModal.classList.add('is-active')
   })
 }
 
@@ -55,11 +80,22 @@ for (const tr of modalTr) {
 
 for (const backBtn of backBtns) {
   backBtn.addEventListener('click', () => {
-    overlay.classList.remove('is-active')
-    body.classList.remove('modal-open')
-
-    for (const modal of modals) {
-      modal.classList.remove('is-active')
+    let isActive = false
+    if (againEditBoardModal) {
+      for (const className of againEditBoardModal.classList) {
+        if (className == 'is-active') {
+          isActive = true
+        }
+      }
+    }
+    if (isActive) {
+      againEditBoardModal.classList.remove('is-active')
+    } else {
+      overlay.classList.remove('is-active')
+      body.classList.remove('modal-open')
+      for (const modal of modals) {
+        modal.classList.remove('is-active')
+      }
     }
   })
 }
@@ -96,8 +132,8 @@ overlay.addEventListener('click', () => {
 //   overlay.classList.toggle('is-active')
 // })
 
-if (completeModal) {
-  overlay.classList.add('is-active')
-  completeModal.classList.add('is-active')
-  body.classList.add('modal-open')
-}
+// if (completeModal) {
+//   overlay.classList.add('is-active')
+//   completeModal.classList.add('is-active')
+//   body.classList.add('modal-open')
+// }
